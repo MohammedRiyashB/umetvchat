3333554d8f920d785d9f8f0b6558c9753e17b220
Mohammed Riyash B <mohdriyash10@gmail.com>
2026-09-20 20:44:16 +0530

    Fix stale TURN reference in realtime server

--- server.ts
+++ server.ts
@@ -370,8 +370,7 @@ async function startServer() {
     socket.emit("webrtc_config", {
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' },
-        ...turnServers
+        { urls: 'stun:stun1.l.google.com:19302' }
       ]
     });
 
